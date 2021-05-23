a0a9f7b72e7a8014454a9263bf42ff30cd6978fc
Mohamed <mohamedelyousfi87@gmail.com>
2021-05-23 22:21:25 +0200

    Query is now executed in async task to avoid blocking

--- src/mohamed205/Voicify/task/SendPlayerDistanceTask.php
+++ src/mohamed205/Voicify/task/SendPlayerDistanceTask.php
@@ -6,6 +6,7 @@ namespace mohamed205\Voicify\task;
 
 use mohamed205\Voicify\math\Distance;
 use mohamed205\Voicify\math\DistanceMatrix;
+use pocketmine\scheduler\AsyncTask;
 use pocketmine\scheduler\Task;
 use pocketmine\Server;
 use pocketmine\utils\Internet;
@@ -16,19 +17,31 @@ class SendPlayerDistanceTask extends Task
     public function onRun(int $currentTick)
     {
         foreach (Server::getInstance()->getOnlinePlayers() as $player){
-            var_dump("abc");
             $distanceMatrix = new DistanceMatrix();
             foreach ($player->getLevel()->getPlayers() as $levelPlayer)
             {
                 if($levelPlayer !== $player)
                 {
-                    $distanceMatrix->add($levelPlayer, $player->distance($levelPlayer));
+                    $distanceMatrix->add($levelPlayer->getName(), $player->distance($levelPlayer));
                 }
             }
-            $distance = new Distance($player, $distanceMatrix);
-            var_dump("request sent?");
+            $distance = new Distance($player->getName(), $distanceMatrix);
             var_dump((string) $distance);
-            Internet::getURL("localhost/api/distances?coordinates=" . $distance);
+
+            Server::getInstance()->getAsyncPool()->submitTask(new class($distance) extends AsyncTask {
+
+                private $distance;
+
+                public function __construct($distance)
+                {
+                    $this->distance = $distance;
+                }
+                public function onRun()
+                {
+                    Internet::postURL("localhost/api/distances", "coordinates=" . $this->distance);
+                }
+            });
+
         }
     }
 
