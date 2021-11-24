f127b90d4c4e30eadf7b7988d92097b64d347920
Mohamed <mohamedelyousfi87@gmail.com>
2021-11-24 21:41:56 +0100

    add voice link command (wip config)

--- src/mohamed205/Voicify/Voicify.php
+++ src/mohamed205/Voicify/Voicify.php
@@ -9,10 +9,17 @@ use mohamed205\Voicify\libs\LibSkin\SkinConverter;
 use mohamed205\Voicify\socket\Connector;
 use mohamed205\Voicify\socket\SocketThread;
 use mohamed205\Voicify\task\SendPlayerDistanceTask;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\entity\Skin;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\Server;
+use pocketmine\utils\Internet;
+use pocketmine\utils\TextFormat;
 
 class Voicify extends PluginBase implements Listener
 {
@@ -28,6 +35,34 @@ class Voicify extends PluginBase implements Listener
         $thread->start();
 
         self::$connector = new Connector($thread);
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        if($command->getName() == "voicelink") {
+            if(!$sender instanceof Player)
+            {
+                $sender->sendMessage(TextFormat::RED . "Please run this command in-game.");
+                return true;
+            }
+            $this->getServer()->getAsyncPool()->submitTask(new class ($sender->getName()) extends AsyncTask {
+                public function __construct(public string $player) {}
+
+                public function onRun(): void{
+                    $response = Internet::getURL("localhost/askcode?username={$this->player}&auth=gelebananenzijnkrom");
+                    $this->setResult(json_decode($response->getBody(), true)["code"]);
+                }
+
+                public function onCompletion(): void{
+                    $player = Server::getInstance()->getPlayerExact($this->player);
+                    if($player !== null) {
+                        $player->sendMessage(TextFormat::GREEN . "Uw verificatiecode is " . $this->getResult());
+                    }
+                }
+            });
+            return true;
+        }
+        return false;
     }
 
     /**
